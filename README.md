53ad9a36df719050762502e454be1917ce0193e5
Robin Joseph <robinjoseph08@sbcglobal.net>
2015-06-16 20:07:27 -0700

    remove colors and refine tick_handler

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # Blue Orbit
 
 A simple Pebble Time watch face that shows the hours and minutes numerically and
-represents the seconds by the position of the small orbiting circle. It also
-alternates the color scheme between blue, green, and red every minute.
+represents the seconds by the position of the small orbiting circle.
 
 ## Screenshots
 
-![Blue Screenshot](resources/images/screenshots/blue.png)
-![Green Screenshot](resources/images/screenshots/green.png)
-![Red Screenshot](resources/images/screenshots/red.png)
+![Screenshot](resources/images/screenshots/blue.png)
+
+## Notes
+
+Originally, I had the color of the background shift between blue, green, and red
+every minute, but it wasn't very battery efficient so I removed it.
 
 ## License
 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <pebble.h>
 
 // ============
-// == macros ==
+// == colors ==
 // ============
-#define LENGTH(a) (sizeof(a) / sizeof((a)[0]))
+#define MAIN_COLOR (GColor8) { .argb = 0b11000111 }
+#define SHADOW_COLOR (GColor8) { .argb = 0b11000010 }
 
 // ===============
 // == constants ==
@@ -32,9 +33,6 @@ static Layer *s_small_circle_layer;
 static Layer *s_small_circle_shadow_layer;
 // font
 static GFont s_time_font;
-// colors
-static GColor8 s_colors[6];
-static int s_color_index;
 // animations
 static PropertyAnimation *s_small_circle_property_animation;
 static PropertyAnimation *s_small_circle_shadow_property_animation;
@@ -51,12 +49,10 @@ static GPoint calculate_small_circle_position(tm *tick_time) {
   return pos;
 }
 
-static void update_time() {
+static void update_minute(struct tm *tick_time) {
   // =======================
   // == update time layer ==
   // =======================
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
   // create a long-lived buffer
   static char buffer[] = "00:00";
   // write the current hours and minutes into the buffer
@@ -70,14 +66,9 @@ static void update_time() {
   // set time on layer
   text_layer_set_text(s_time_layer, buffer);
   text_layer_set_text(s_time_shadow_layer, buffer);
+}
 
-  // ===================
-  // == update colors ==
-  // ===================
-  s_color_index = tick_time->tm_min % (LENGTH(s_colors) / 2);
-  window_set_background_color(s_main_window, s_colors[s_color_index * 2]);
-  text_layer_set_text_color(s_time_shadow_layer, s_colors[s_color_index * 2 + 1]);
-
+static void update_second(struct tm *tick_time) {
   // ==========================
   // == animate small circle ==
   // ==========================
@@ -98,7 +89,12 @@ static void update_time() {
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  if ((units_changed & SECOND_UNIT) == SECOND_UNIT) {
+    update_second(tick_time);
+  }
+  if ((units_changed & MINUTE_UNIT) == MINUTE_UNIT) {
+    update_minute(tick_time);
+  }
 }
 
 // ===============================
@@ -106,10 +102,10 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 // ===============================
 static void small_circle_layer_draw(Layer *layer, GContext *ctx) {
   if (layer == s_small_circle_shadow_layer) {
-    graphics_context_set_fill_color(ctx, s_colors[s_color_index * 2 + 1]);
-    graphics_context_set_stroke_color(ctx, s_colors[s_color_index * 2 + 1]);
+    graphics_context_set_fill_color(ctx, SHADOW_COLOR);
+    graphics_context_set_stroke_color(ctx, SHADOW_COLOR);
   } else {
-    graphics_context_set_fill_color(ctx, s_colors[s_color_index * 2]);
+    graphics_context_set_fill_color(ctx, MAIN_COLOR);
     graphics_context_set_stroke_color(ctx, GColorWhite);
   }
   graphics_context_set_stroke_width(ctx, STROKE_WIDTH);
@@ -126,7 +122,7 @@ static void circle_layer_draw(Layer *layer, GContext *ctx) {
   const int16_t half_h = bounds.size.h / 2;
   GPoint pos;
   if (layer == s_circle_shadow_layer) {
-    graphics_context_set_stroke_color(ctx, s_colors[s_color_index * 2 + 1]);
+    graphics_context_set_stroke_color(ctx, SHADOW_COLOR);
     pos = GPoint(half_w, half_h + SHADOW_OFFSET);
   } else {
     graphics_context_set_stroke_color(ctx, GColorWhite);
@@ -142,7 +138,7 @@ static void main_window_load(Window *window) {
   // ==================
   s_circle_layer = layer_create(GRect(PADDING, PADDING, WIDTH - PADDING * 2, HEIGHT - PADDING * 2));
   layer_set_update_proc(s_circle_layer, circle_layer_draw);
-  // circle lasyer  shadow
+  // circle lasyer shadow
   s_circle_shadow_layer = layer_create(GRect(PADDING, PADDING + SHADOW_OFFSET, WIDTH - PADDING * 2, HEIGHT - PADDING * 2));
   layer_set_update_proc(s_circle_shadow_layer, circle_layer_draw);
 
@@ -155,7 +151,7 @@ static void main_window_load(Window *window) {
   // time layer shadow
   s_time_shadow_layer = text_layer_create(GRect(0, HEIGHT / 2 - TEXT_HEIGHT / 2 - TEXT_OFFSET + SHADOW_OFFSET + 1, WIDTH, TEXT_HEIGHT));
   text_layer_set_background_color(s_time_shadow_layer, GColorClear);
-  text_layer_set_text_color(s_time_shadow_layer, s_colors[s_color_index * 2 + 1]);
+  text_layer_set_text_color(s_time_shadow_layer, SHADOW_COLOR);
   // font
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MONTSERRAT_36));
   text_layer_set_font(s_time_layer, s_time_font);
@@ -187,18 +183,18 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
-  // Unload GFont
+  // unload GFont
   fonts_unload_custom_font(s_time_font);
 
-  // Destroy TextLayer
+  // destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_time_shadow_layer);
 
-  // Destroy Circle Layer
+  // destroy Circle Layer
   layer_destroy(s_circle_layer);
   layer_destroy(s_circle_shadow_layer);
 
-  // Destroy Small Circle Layer
+  // destroy Small Circle Layer
   layer_destroy(s_small_circle_layer);
   layer_destroy(s_small_circle_shadow_layer);
 
@@ -208,43 +204,34 @@ static void main_window_unload(Window *window) {
 }
 
 static void init() {
-  // ================
-  // == set colors ==
-  // ================
-  s_colors[0] = GColorBlueMoon;
-  s_colors[1] = GColorDukeBlue;
-  s_colors[2] = GColorIslamicGreen;
-  s_colors[3] = GColorDarkGreen;
-  s_colors[4] = GColorFolly;
-  s_colors[5] = GColorBulgarianRose;
-
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
-  // Register with TickTimerService
+  // register with TickTimerService
   tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
 
-  // Create main Window element and assign to pointer
+  // create main Window element and assign to pointer
   s_main_window = window_create();
 
-  s_color_index = tick_time->tm_min % (LENGTH(s_colors) / 2);
-  window_set_background_color(s_main_window, s_colors[s_color_index * 2]);
+  // set background color
+  window_set_background_color(s_main_window, MAIN_COLOR);
 
-  // Set handlers to manage the elements inside the Window
+  // set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
 
-  // Show the Window on the watch, with animated=true
+  // show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 
-  // Make sure the time is displayed from the start
-  update_time();
+  // make sure the time is displayed from the start
+  update_second(tick_time);
+  update_minute(tick_time);
 }
 
 static void deinit() {
-  // Destroy Window
+  // destroy Window
   window_destroy(s_main_window);
 }
 
